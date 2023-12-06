96015af9485b3434d37800ce83d6defedcd2a82d
Martin Garbuz <martin@progmatic.ee>
2023-12-06 21:44:09 +0200

    day 6 part 2

--- day6.js
+++ day6.js
@@ -2,38 +2,33 @@ const input = getRealInput();
 
 const lines = input.split('\n')
 
-const times = []
-const distances = []
+let time = ''
+let distance = ''
 
 let result = 1
 
 for (const line of lines) {
     const parts = line.split(':')
     let title = parts[0];
-    let values = parts[1].trim().split(/\s+/).map(v => parseInt(v));
+    let values = parts[1].replaceAll(' ', '')
     if (title === 'Time') {
-        times.push(...values)
+        time += values
     } else if (title === 'Distance') {
-        distances.push(...values)
+        distance += values
     }
 }
 
-for (let i = 0; i < times.length; i++) {
-    const time = times[i]
-    const currentRecord = distances[i]
-    let possibleWins = 0;
-    for (let j = 1; j < time; j++) {
-        const speed = j;
-        const driveTime = time - j;
-        const totalDistance = driveTime * speed;
-        if (totalDistance > currentRecord) {
-            possibleWins++;
-        }
+let possibleWins = 0;
+for (let j = 1; j < time; j++) {
+    const speed = j;
+    const driveTime = time - j;
+    const totalDistance = driveTime * speed;
+    if (totalDistance > distance) {
+        possibleWins++;
     }
-    result *= possibleWins;
 }
 
-console.log(result)
+console.log(possibleWins)
 
 function getTestInput() {
     return `Time:      7  15   30
